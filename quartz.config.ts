21c660e26d01cf7fa64cd6233d4da3ee1857fd04
Frédéric Bardolle <seiteta@users.noreply.github.com>
2026-01-19 22:28:20 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "f10e.eu",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "published",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
